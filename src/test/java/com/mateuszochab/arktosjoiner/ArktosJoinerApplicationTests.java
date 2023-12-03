a2bfe91a3ace8fd3f4f905f4fe5e0386c96586dc
ochab.mat@gmail.com <ochab.mat@gmail.com>
2023-12-03 20:32:10 +0100

    initial commit

---
+++ src/test/java/com/mateuszochab/arktosjoiner/ArktosJoinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszochab.arktosjoiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArktosJoinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
