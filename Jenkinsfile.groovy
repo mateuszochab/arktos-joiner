e63f761d4aa75a1abcca12b1b2f54e49ff572eee
Mateusz Ochab <ochab.mat@gmail.com>
2023-12-13 20:59:52 +0100

    add dockerfile and jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    def dockerImage = docker.build("arktos-joiner-0.0.1-SNAPSHOT")
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                // Wysyłanie obrazu Dockerowego do repozytorium Docker Hub lub innych
+                script {
+                    docker.withRegistry('https://hub.docker.com', 'credentials-dockerhub') {
+                        def dockerImage = docker.build("arktos-joiner-0.0.1-SNAPSHOT")
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Czyszczenie po zakończeniu budowy
+            cleanWs()
+        }
+    }
+}
